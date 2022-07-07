0cd36c494be2ad51eb457086e6180608fbe440ef
weslleyrods <weslley.rdgs@gmail.com>
2022-07-07 14:07:56 -0300

    feat: implementa mensagem de erro no express-flash

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,8 +8,13 @@ exports.add = (req, res)=>{
 exports.addAction = async (req, res)=>{
     //res.json(req.body)
     const post = new Post(req.body);
-    await post.save();   
-    req.flash('success', 'Post salvo com sucesso!')
-    //req.flash('error', 'Erro ao salvar o post!')
+
+    try{
+        await post.save();   
+        req.flash('success', 'Post salvo com sucesso!');
+    }catch(error){
+        req.flash('error', 'Erro: '+error.message);
+        return res.redirect('/post/add')    
+    }
     res.redirect('/');
-};
+}; 
