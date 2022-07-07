3cb6a432551590145935c1aef5fd53d442ac9bfd
weslleyrods <weslley.rdgs@gmail.com>
2022-07-07 17:40:04 -0300

    feat: implementa slug aos posts com tratamento de case-insensitive

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,4 @@
+const slug = require('slug');
 const mongoose = require('mongoose');
 //cria a comunicação entre o mongodb e a aplicação, passando a promise do próprio navegador
 mongoose.Promise  = global.Promise; 
@@ -14,6 +15,13 @@ const postSchema = new mongoose.Schema({
         trim: true,
     },
     tags: [String]
+});
+
+postSchema.pre('save',function(next){
+    if(this.isModified('title')){
+        this.slug = slug(this.title, {lower:true});
+    }
+    next();
 });
 
 module.exports = mongoose.model('Post', postSchema);
